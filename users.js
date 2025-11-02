fef8ad87dc6a339e6ef46f64d4dcfb41a6a0ed3d
theAliyev75 <esahib1975@gmail.com>
2025-11-02 17:45:05 +0400

    DATABASE

---
+++ users.js
@@ -0,0 +1,14 @@
+let data = [
+    {
+        "id": 1041,
+        "ad": "Ilqar",
+        "soyad": "Farhadov",
+        "yas": 19
+    },
+    {
+        "id": 5784,
+        "ad": "Sahib",
+        "soyad": "Aliyev",
+        "yas": 20
+    }
+]
